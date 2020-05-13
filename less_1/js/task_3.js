2b9e7d644c2e37e5c3066fc35c1f93d176d2ae10
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-05-13 19:30:50 +0300

    less_1

--- less_1/js/task_3.js
+++ less_1/js/task_3.js
@@ -1,7 +1,8 @@
 function myFunction3(){
     document.getElementById('paragraph').innerHTML = '<p>3.	Чему будет равно JS-выражение 1000 + "108"?<br><br>let a = 1000;<br>let b = "108";<br><br>result = a + b;<br>console.log(result);<br><br>Результат: <b>1000108</b><br>Результат (конкатенация) обусловлен тем, что в переменные переданы разные типы данных</p>';
-    let a = 1000;
-    let b = "108";
+    let a, b;
+    a = 1000;
+    b = "108";
     result = a + b;
     console.log(result);
 }
