02d96f67b383cbc21d6b2510529db5a3eee05391
mutedalien <chel_c@mail.ru>
2020-05-13 19:40:11 +0300

    less_1

--- less_1/js/task_2.js
+++ less_1/js/task_2.js
@@ -1,5 +1,5 @@
 function myFunction2(){
-    document.getElementById('paragraph').innerHTML = '2.	Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать значение из name в admin. Вывести admin (должно вывестись «Василий»).<br><br>let admin, name;<br>name = \'Василий\';<br>admin = name;<br>console.log(admin);<br><br> Загляните в консоль';
+    document.getElementById('paragraph').innerHTML = '2.	Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать значение из name в admin. Вывести admin (должно вывестись «Василий»).<br><br>let admin, name;<br>name = \'Василий\';<br>admin = name;<br><br>console.log(admin);<br><br> Загляните в консоль';
     let admin, name;
     name = 'Василий';
     admin = name;
--- less_1/js/task_3.js
+++ less_1/js/task_3.js
@@ -1,5 +1,5 @@
 function myFunction3(){
-    document.getElementById('paragraph').innerHTML = '<p>3.	Чему будет равно JS-выражение 1000 + "108"?<br><br>let a = 1000;<br>let b = "108";<br><br>result = a + b;<br>console.log(result);<br><br>Результат: <b>1000108</b><br>Результат (конкатенация) обусловлен тем, что в переменные переданы разные типы данных</p>';
+    document.getElementById('paragraph').innerHTML = '<p>3.	Чему будет равно JS-выражение 1000 + "108"?<br><br>let a, b;<br> a = 1000;<br>b = "108";<br><br>result = a + b;<br>console.log(result);<br><br>Результат: <b>1000108</b><br>Результат (конкатенация) обусловлен тем, что в переменные переданы разные типы данных</p>';
     let a, b;
     a = 1000;
     b = "108";
