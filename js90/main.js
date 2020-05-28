124d8cdfa7dc88560bb3be295acc822a767a3049
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-05-28 10:58:29 +0300

    ОБЪЕКТЫ

--- js90/main.js
+++ js90/main.js
@@ -129,6 +129,36 @@ myObject.name = "Петя"; // меняем Васю на Петю
 console.log(myObject.name);
 console.log(myObject.getFullName());
 
+// ЕЩЕ ОБЪЕКТЫ
+var chevy = {
+	make: "Chevy",
+	model: "Aveo",
+	year: 2011,
+	color: "red",
+	passengers: 4,
+	convertible: false,
+	mileage: 1021
+	};
+
+var cadi = {
+	make: "Cadillac",
+	model: "GM",
+	year: 1955,
+	color: "yellow",
+	passengers: 2,
+	convertible: true,
+	mileage: 12821
+};
+
+var newObj = {};
+
+delete cadi.passengers;
+
+cadi.state = "normal";
+console.log(cadi);
+
+console.log(cadi.passengers);
+
 // КЛАССЫ
 class Hater {
     constructor(firstName, lastName) {
